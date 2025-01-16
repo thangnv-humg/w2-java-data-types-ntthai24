6d496c44ace7817fa81749b8e49b99733e717461
ntthai24 <nguyenhoangphithai1186@gmail.com>
2025-01-16 17:14:15 +0700

    BT 16/01/2025

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
+        // Bài 1: 
+        int a = 42;
+        double b = 3.14;
+        String c = "Hello, Java!";
+
+        System.out.println("Giá trị của biến int là: " + a);
+        System.out.println("Giá trị của biến double là:" + b);
+        System.out.println("Giá trị của biến String là:" + c);
+        
         // Ví dụ:
         // int myInt = 42;
         // double myDouble = 3.14;
@@ -9,5 +18,13 @@ public class Main {
         // System.out.println(myInt);
         // System.out.println(myDouble);
         // System.out.println(myString);
+
+        // Bài 2:
+        if (a > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Less or equal to 10");
+        }
     }
 }
+
